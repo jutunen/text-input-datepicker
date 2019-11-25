c9b164825cc8dfa45c0de93b786e88694447a57b
jutunen <u-jussi@suomi24.fi>
2019-11-25 17:39:47 +0200

    removed commented code

--- dist/template_minifier.js
+++ dist/template_minifier.js
@@ -1,21 +1,8 @@
 var fs = require("fs");
-
 var args = process.argv.slice(2);
-
-//console.log(JSON.stringify(args));
-
 var data = fs.readFileSync(args[0]);
-
-//console.log(data.toString());
-
-//var newStr = data.toString().replace(/`(.|\n)*`/,"Heh");
-//var newStr = data.toString().replace(/(`)\n(<style>)/,"$1$2");
-//var newStr = data.toString().replace(/(.*;)\n(.*)/g,"$1$2");
 var newStr = data.toString().replace(/\\n\s*/g,"");
 newStr = newStr.replace(/\s{/g,"{");
-
-//console.log(newStr);
-
 fs.writeFile(args[1], newStr, function (err) {
   if (err) throw err;
 });
