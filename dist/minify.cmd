24aced108db4850544569c55e5cf1aa045364f89
jutunen <u-jussi@suomi24.fi>
2019-11-25 16:37:08 +0200

    initial dist folder commit

---
+++ dist/minify.cmd
@@ -0,0 +1,6 @@
+terser text-input-datepicker.js -c -m --mangle-props reserved=[constructor,observedAttributes,disconnectedCallback,attributeChangedCallback,Datepicker,connectedCallback,enable,disable,setFocusOnCal,getDateString,getDateObject,dayNames,monthNames,sundayFirst,persistOnSelect,ignoreOnFocus,initDate] -o _temp-file.js
+node template_minifier.js _temp-file.js text-input-datepicker.min.js
+rm _temp-file.js
+terser text-input-datepicker-node.js -c -m --mangle-props reserved=[constructor,observedAttributes,disconnectedCallback,attributeChangedCallback,Datepicker,connectedCallback,enable,disable,setFocusOnCal,getDateString,getDateObject,dayNames,monthNames,sundayFirst,persistOnSelect,ignoreOnFocus,initDate] -o _temp-file.js
+node template_minifier.js _temp-file.js text-input-datepicker-node.min.js
+rm _temp-file.js
